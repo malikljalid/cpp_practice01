d3a6fea1a9f3fb316ff9cc644ac65036f3a3eee6
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-16 22:08:30 +0100

    practice using custom libraries

---
+++ myOwnLibs.cpp
@@ -0,0 +1,10 @@
+#include "myMathLibrary.hpp"
+#include "myInputLibrary.hpp"
+
+int main(void)
+{
+    int myNumber        = num::readNumber();
+    int randomNumber    = num::randomNumber(1, 9);
+
+    return (0);
+}
