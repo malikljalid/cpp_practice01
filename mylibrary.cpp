d0e3dd8ef7243862d7212a16fe18d93a1bee2360
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-16 22:07:59 +0100

    cpp program to call my hpp library

---
+++ mylibrary.cpp
@@ -0,0 +1,12 @@
+#include "mylibrary.hpp"
+
+int main(void)
+{
+    std::cout << "Hello world\n";
+
+    helloWorld("Hello Zakaria");
+
+    std::cout << tools::sumOfTwo(10, 3) << std::endl;
+
+    return (0);
+}
