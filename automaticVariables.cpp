640703e2f3226030d8001ed68fb58b8612860319
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-19 14:22:08 +0100

    automatic variables discovered, they are boring btw

---
+++ automaticVariables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+    auto x = 13;                        // auto set to int
+    auto y = 42.01;                     // auto set to float
+    auto z = "Zakaria The Monster";     // auto set to string
+
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+    std::cout << z << std::endl;
+
+    return (0);
+}
