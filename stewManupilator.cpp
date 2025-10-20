86ba16e89436d4b77ea687c10dd0454da0e66fe5
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-20 13:42:21 +0100

    stew manupilator is useful for printing cool headers, btw stew is not a function.. its a MANUPILATOR

---
+++ stewManupilator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+
+int main(void)
+{
+    std::cout << "---------|--------------------------------|---------|" << std::endl;
+    std::cout << "  Code   |               Name             |  Mark   |" << std::endl;
+    std::cout << "---------|--------------------------------|---------|" << std::endl;
+
+    std::cout << std::setw(9) << "C101" << "|" << std::setw(32) << "Introduction to Programming 1" << "|" << std::setw(9) << "95" << "|" << std::endl;
+    std::cout << std::setw(9) << "C102" << "|" << std::setw(32) << "Computer Hardware" << "|" << std::setw(9) << "88" << "|" << std::endl;
+    std::cout << std::setw(9) << "C1035243" << "|" << std::setw(32) << "Network"<< "|" <<  std::setw(9) <<"75" << "|" << std::endl;
+    std::cout << "---------|--------------------------------|---------|" << std::endl; 
+}
