d95eacd34751d47f960ad7c10bb8e7ecd457b579
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-22 12:54:44 +0100

    vectors with structs are a way more tadious of using data

---
+++ vectors_ofStructre.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+struct stEmployee
+{
+    std::string     FirstName;
+    std::string     LastName;
+    int             Salary;
+};
+
+stEmployee setEmployee(std::string FirstName, std::string LastName, int Salary)
+{
+    stEmployee Employee;
+
+    Employee.FirstName = FirstName;
+    Employee.LastName  = LastName;
+    Employee.Salary    = Salary;
+
+    return (Employee);
+}
+
+void printEmployee(stEmployee &Employee)
+{
+    std::cout << "First Name : " << Employee.FirstName << std::endl;
+    std::cout << "Last Name  : " << Employee.LastName << std::endl;
+    std::cout << "Salary     : " << Employee.Salary << std::endl;
+    std::cout << std::endl;
+}
+
+int main(void)
+{
+    std::vector <stEmployee>    Employees;
+    stEmployee                  tmpEmployee;
+
+    tmpEmployee = setEmployee("Zachariah", "The killer", 7000);
+    Employees.push_back(tmpEmployee);
+
+    tmpEmployee = setEmployee("Jack", "Lacroix", 100000);
+    Employees.push_back(tmpEmployee);
+
+    tmpEmployee = setEmployee("Lionel", "Andreas Messi", 800000);
+    Employees.push_back(tmpEmployee);
+
+    Employees.push_back(setEmployee("Pep", "Guardiola", 40000));
+    Employees.push_back(setEmployee("Jose", "Murinho", 20000));
+
+    for (stEmployee &Employee : Employees)
+        printEmployee(Employee);
+
+    return (0);
+}
