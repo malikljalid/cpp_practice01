a9265290128eec517a0d013a3184304c33fa342b
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-25 11:44:31 +0100

    new and delete can be used with arrays too, lol

---
+++ dynamicAllocation_Arrays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(void)
+{
+    int     *StudentGrade;
+    int     StudentsNumber;
+
+    std::cout << "Please enter the number of Students : ";
+    std::cin >> StudentsNumber;
+
+    StudentGrade = new int[StudentsNumber];
+
+    for (int i = 0; i < StudentsNumber; i++)
+    {
+        std::cout << "Please enter Student " << i + 1 << " Grade : ";
+        std::cin >> StudentGrade[i];
+    }
+
+    for (int i = 0; i < StudentsNumber; i++)
+        std::cout << "Student " << i + 1 << " Grade : " << StudentGrade[i] << std::endl;
+
+    return (0);
+}
